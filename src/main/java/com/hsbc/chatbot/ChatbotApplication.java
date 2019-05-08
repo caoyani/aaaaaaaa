62523fe85e1bc28bf10a40507aac8965718da6dc
caoyani <476056943@qq.com>
2019-05-08 22:44:09 +0800

    vo

--- src/main/java/com/hsbc/chatbot/ChatbotApplication.java
+++ src/main/java/com/hsbc/chatbot/ChatbotApplication.java
@@ -1,5 +1,8 @@
 package com.hsbc.chatbot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hsbc.chatbot.vo.PolicyVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +18,21 @@ public class ChatbotApplication {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello, Nicole";
+		return "Hello, Chatbot";
 	}
 
-	@RequestMapping("/chatbot")
+	@RequestMapping("/policyQuery")
 	public String chatbot() {
-		return "Hello, Chatbot";
+		PolicyVO policyVO = new PolicyVO();
+		// Convert object to JSON string
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonStr = "";
+		try {
+			jsonStr = mapper.writeValueAsString(policyVO);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return jsonStr;
 	}
 
 
